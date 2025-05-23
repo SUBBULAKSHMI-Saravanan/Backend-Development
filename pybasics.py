27620cfeeec88dd47a48967043f628b0db515631
Subbulakshmi <selvisara93@gmail.com>
2025-05-23 10:02:03 +0530

    first commit

---
+++ pybasics.py
@@ -0,0 +1,42 @@
+#string examples
+print('doesn\'t' ) 
+print("doesn't" ) 
+
+print('"Yes," they said.')
+print("\"Yes,\" they said.")
+
+print('"Isn\'t," they said.')
+print("hello\' world")
+print('\"yes,\"' "hello every one")
+
+s="firstline. \n secondline"
+print(s)
+
+print("C:same\name ")
+print(r'C:same\name')
+
+print("""\...Usage:Hello
+      ...-h
+      ...-o""")
+print("""hiii 
+hloo
+welcome""")
+
+p=3*"ABC"+"DEF"
+print(p)
+#posistions
+word="python"
+word[0]
+print(word[0])
+print(word[:2])
+print(word[4:])
+print(word[-2])
+print(word[:4]+word[4:])
+#list
+list=["python","java","C","HTML","CSS"]
+print(list)
+square=[4,6,4,82,45,23,89]
+print(square)
+print(square[5])
+print(square[-3:])
+print(square[5:])
